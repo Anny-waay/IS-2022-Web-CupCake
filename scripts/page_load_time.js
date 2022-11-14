378e70ce5b31e774deb38e86109cb2d55a384d42
ann.komova <ann.komova@gmail.com>
2022-11-14 14:21:18 +0300

    fix: time load script

--- scripts/page_load_time.js
+++ scripts/page_load_time.js
@@ -1,8 +1,8 @@
 (() => {
-    const loadStart = new Date().getTime();
+    let time = performance.timing;
 
     window.addEventListener('load', () => {
         const timeStamp = document.querySelector('#timestamp');
-        timeStamp.innerHTML += `Время загрузки страницы: ${(new Date().getTime() - loadStart) / 1000} секунд`;
+        timeStamp.innerHTML += `Время загрузки страницы: ${(time.loadEventStart - time.navigationStart) / 1000} секунд`;
     });
 })();
