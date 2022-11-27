91b5959413d475a0846f2b1458e408af27b46be5
ann.komova <ann.komova@gmail.com>
2022-11-27 19:30:46 +0300

    fix: display location script

--- scripts/display_location.js
+++ scripts/display_location.js
@@ -1,6 +1,11 @@
-try {
-    const pathname = document.location.pathname
-    const navItem = document.querySelector(`[data-nav="${pathname}"]`)
-    navItem.classList.add("current-location")
+function displayLocation(id){
+    try{
+        const item=document.getElementById(id).getElementsByTagName('a');
+        const href=document.location.href;
+        for(let i=0; i<item.length; i++){
+            if (href === item[i].href){
+                item[i].classList.add("current-location");
+            }
+        }
+    } catch(e){}
 }
-catch (e) {}
